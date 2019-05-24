51175fdb3befeb3a33a7eb3640a1ae27a1237199
Meows Ma <Msilent@live.com>
2019-05-24 14:06:27 +0800

    Implement advanced login

---
+++ test/helpers/sessions_helper_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+class SessionsHelperTest < ActionView::TestCase
+  def setup
+    @user = users(:michael)
+    remember(@user)
+  end
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
+  end
+  test "current_user returns nil when remember digest is wrong" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
+  # 返回 cookie 中记忆令牌对应的用户
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+  
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,9 +6,22 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
+  # 判断是否登陆
   def is_logged_in?
     !session[:user_id].nil?
   end
+  # 登陆指定用户
+  def log_in_as(user)
+    sessopm[:user_id]=user.id
+  end
 
   # Add more helper methods to be used by all tests here...
+end
+class ActionDispatch::IntegrationTest
+  # 登入指定的用户
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
