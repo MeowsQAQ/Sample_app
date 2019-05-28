068a9ca7ea53d782748c14d746702a1ec97b5969
Meows Ma <Msilent@live.com>
2019-05-28 15:11:35 +0800

    Add password reset with mail

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get '/about',to:'static_pages#about'
   get '/contact',to:'static_pages#contact'
   get '/login' , to: 'sessions#new'
-  post 'login' , to: 'sessions#create'
+  post '/login' , to: 'sessions#create'
   delete '/logout' , to: 'sessions#destory'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new,:create,:edit,:update]
 end
