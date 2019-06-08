b2392df0256cf57da57858afff7937f5ee600b1c
Meows Ma <Msilent@live.com>
2019-06-08 12:27:25 +0800

    bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
 
   def following
     @title = "Following"
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     @users = @user.following.paginate(page:params[:page])
     render 'show_follow'
   end
